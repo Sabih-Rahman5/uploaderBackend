11e6089c0a85b94756dff296547a5039da3b7eb5
Sabih Rahamn <sabih.562@gmail.com>
2025-11-10 21:32:04 +0500

    init commit

--- backend/settings.py
+++ backend/settings.py
@@ -128,3 +128,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = ['annually-star-shrew.ngrok-free.app', 'localhost', '127.0.0.1']
