fe6ee26bc81ea202834738380dd56ee3a61077f6
Sabih Rahamn <sabih.562@gmail.com>
2025-11-17 00:41:43 +0500

    prgress label

--- uploader/views.py
+++ uploader/views.py
@@ -2,10 +2,10 @@ import os
 import uuid
 
 from django.conf import settings
-from django.conf.urls.static import static
 from django.http import JsonResponse
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -45,19 +45,16 @@ def UploadAssignment(request):
 
     return JsonResponse({"error": "Invalid request method"}, status=405)
 
-@csrf_exempt
+@api_view(['POST'])
 def RunInference(request):
-    if request.method == 'POST':
-        modelManager = GPUModelManager.getInstance()
-        if modelManager.getLoadedModel == "None":
-            return JsonResponse({"error": "Select a model!!"}, status=400)
-        if modelManager.assignmentPath == "":
-            return JsonResponse({"error": "No Assignment Uploaded!!"}, status=400)
-        
-        if modelManager.runInference():
-            return Response({'status': 'ok'}, status=200)
-            
-        return Response({"error": "No model selected"}, status=status.HTTP_400_BAD_REQUEST)
+    modelManager = GPUModelManager.getInstance()
+    if modelManager.getLoadedModel == "None":
+        return Response({"error": "Select a model!!"}, status=status.HTTP_400_BAD_REQUEST)
+    if modelManager.assignmentPath == "":
+        return Response({"error": "No Assignment Uploaded!!"}, status=status.HTTP_400_BAD_REQUEST)
+    if modelManager.runInference():
+        return Response({'status': 'ok'}, status=status.HTTP_200_OK)
+    return Response({"error": "No model selected"}, status=status.HTTP_400_BAD_REQUEST)
     
     
     
