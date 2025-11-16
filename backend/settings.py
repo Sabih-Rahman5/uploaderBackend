81d95a743ab5bf9fe74a6dfbc8f5dccc8380ec46
Sabih Rahamn <sabih.562@gmail.com>
2025-11-16 23:27:19 +0500

    prgress label

--- backend/settings.py
+++ backend/settings.py
@@ -129,13 +129,16 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-ALLOWED_HOSTS = ['annually-star-shrew.ngrok-free.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
 ]
+
 
 
 CORS_ALLOW_HEADERS = [
