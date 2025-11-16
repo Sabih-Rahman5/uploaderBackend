66ec98a354af54be4e1e53994018ef249c11f027
Sabih Rahamn <sabih.562@gmail.com>
2025-11-16 20:19:03 +0500

    prgress label

--- uploader/urls.py
+++ uploader/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import UploadAssignment, UploadKnowledgebase, AssignmentList, GetAssignmentText, UpdateAssignmentText, RunModel, ExampleView
+from .views import UploadAssignment, UploadKnowledgebase, AssignmentList, GetAssignmentText, UpdateAssignmentText, RunModel, ExampleView, ModelStatus
 
 
 urlpatterns = [
